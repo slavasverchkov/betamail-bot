738abc127fd46e9ad3f2930316b7e454231eef49
vsverchkov <vsverchkov@zuzex.com>
2018-09-21 00:11:10 +0300

    first commit

---
+++ routes/web.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::middleware(['auth'])->prefix('admin')->namespace('Backend')->name('admin.')->group( function (){
+    Route::get('/', 'DashboardController@index')->name('index');
+});
+
+Route::get('/setting', 'SettingController@index')->name('setting.index');
+Route::get('/setting/store', 'SettingController@store')->name('setting.store');
+
+Route::middleware(['auth'])->prefix('admin')->namespace('Backend')->name('admin.')->group(function (){
+	Route::get('/', 'DashboardController@index')->name('index');
+
+	Route::get('/setting', 'SettingController@index')->name('setting.index');
+	Route::post('/setting/store', 'SettingController@store')->name('setting.store');
+        
+        Route::post('setting/setwebhook', 'SettingController@setwebhook')->name('setting.setwebhook');
+	Route::post('setting/getwebhookinfo', 'SettingController@getwebhookinfo')->name('setting.getwebhookinfo');
+});
+
+
+Auth::routes();
+
+Route::match(['post', 'get'], 'register', function() {
+    Auth::logout();
+    return redirect('/'); 
+})->name('register');
+
+
+Route::get('/home', 'HomeController@index')->name('home');
